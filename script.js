64f831a94d644caa908073c2ab31ad027cfe40a5
KartikHU890 <kc415822@gmail.com>
2026-02-11 02:01:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function pageOne() {
   playMusic();
   clear();
   app.innerHTML = `
-    <img src="ishika.jpeg"> 
+    <img src="ishika.jpeg"/> 
     <h1>HELLOOO ISHIKAA 💗</h1>
     <p>I made something special…<br>Just for you 🌸</p>
     <button onclick="pageTwo()">Okay…????</button>
@@ -77,6 +77,7 @@ function pageFive() {
 }
 
 pageOne();
+
 
 
 
